647592ff1132a9bd9f594a74caa4bf8c699151b1
jiangtian <284053253@qq.com>
2018-11-08 22:19:22 +0800

    ts

--- vendor/yiisoft/yii2/web/Application.php
+++ vendor/yiisoft/yii2/web/Application.php
@@ -101,6 +101,7 @@ class Application extends \yii\base\Application
             Yii::debug("Route requested: '$route'", __METHOD__);
             $this->requestedRoute = $route;
             $result = $this->runAction($route, $params);
+            return $result;
             if ($result instanceof Response) {
                 return $result;
             }
