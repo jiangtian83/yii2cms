3b88d8294bc3e8b46a960b7f95ee21ccf14ba02e
jiangtian83 <284053253@qq.com>
2018-11-09 09:10:46 +0800

    ts

--- common/config/main.php
+++ common/config/main.php
@@ -6,17 +6,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-		//memcache缓存组件 我们的框架用到了memcache缓存哦，所以需要配置这个
-		'memcache' => [  
-			'class' => 'yii\caching\MemCache',  
-			'useMemcached' =>0, //这里简单说明一下 0是memcache, 1是memcached 两个是php里不同的扩展
-			'servers' => [  
-				[  
-					'host' => '127.0.0.1',  
-					'port' => 11211,  
-					'weight' => 100,  
-				]
-			],  
-		],
+	//memcache缓存组件 我们的框架用到了memcache缓存哦，所以需要配置这个
+	'memcache' => [  
+		'class' => 'yii\caching\MemCache',  
+		'useMemcached' =>0, //这里简单说明一下 0是memcache, 1是memcached 两个是php里不同的扩展
+		'servers' => [  
+			[  
+				'host' => '127.0.0.1',  
+				'port' => 11211,  
+				'weight' => 100,  
+			]
+		],  
+	],
     ],
 ];
