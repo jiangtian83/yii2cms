fb539f8a4bd5fbf7361ecb7b2b5bce949e27e4e9
jiangtian <284053253@qq.com>
2018-12-23 12:53:36 +0800

    ts

--- backend/views/industry/_form.php
+++ backend/views/industry/_form.php
@@ -16,12 +16,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'pid')->textInput() ?>
 
-    <?= $form->field($model, 'creator')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
