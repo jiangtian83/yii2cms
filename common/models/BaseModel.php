2c9c63bd60eed315d79edb6f9ec984533f6c6b66
jiangtian <284053253@qq.com>
2018-12-23 14:16:47 +0800

    基model

---
+++ common/models/BaseModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2018/12/23
+ * Time: 下午2:14
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+
+namespace common\models;
+
+
+class BaseModel
+{
+
+}
