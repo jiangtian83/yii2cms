303866a4850f26ab0c24dc653f3d0980110adc2a
jiangtian <284053253@qq.com>
2018-12-24 02:16:55 +0800

    提交所有修改及更新

---
+++ common/behaviors/GuidBehavior.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2018/12/23
+ * Time: 下午9:17
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+
+namespace common\behaviors;
+
+
+class GuidBehavior
+{
+
+}
